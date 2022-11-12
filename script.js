cf07d39e5db0fedb5f2186be904aadac20619c40
vitoradsp <vitoradsp@gmail.com>
2022-11-12 20:29:58 -0300

    Countdown added

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-var fodasevtncporra = new Time()
+function countdown(){
+    const acabaQuando = Date.parse('22 Nov 2022 17:00:00');
+    var diaAgora = new Date();
+    let tempoRestante = acabaQuando - diaAgora
+    let timerdias = Math.floor(tempoRestante / (1000 * 60 * 60 * 24));
+    let timerhoras = Math.floor(tempoRestante / (1000 * 60 * 60));
+    let timerminutos = Math.floor(tempoRestante / (1000 * 60));
+    let timersegundos = Math.floor(tempoRestante / 1000);
+    dias = timerdias;
+    horas = timerhoras - dias * 24
+    minutos = timerminutos - timerhoras * 60;
+    segundos = timersegundos - timerminutos * 60;
+    if (segundos<10){
+        segundos = '0'+segundos
+    }
+    document.getElementById('tempoquefalta').innerHTML = `${dias}D ${horas}:${minutos}:${segundos}`
+}
+
+setInterval(countdown, 1000)
